e8d0a84882e1bbad97a9fd9aaa88ff175f4c4fcd
dex4 <cata_dc98@yahoo.com>
2020-03-29 17:31:20 +0300

    Implement addStudent BVA test methods for student id and email

--- src/test/java/Test/AppTest.java
+++ src/test/java/Test/AppTest.java
@@ -1,21 +1,15 @@
 package Test;
 
-import Domain.Nota;
 import Domain.Student;
-import Domain.Teme;
-import Repository.NoteRepo;
 import Repository.StudentRepo;
-import Repository.TemeRepo;
-import Service.ServiceNote;
 import Service.ServiceStudent;
-import Service.ServiceTeme;
 import Validator.StudentValidator;
-import Validator.TemeValidator;
-import Validator.NotaValidator;
+import Validator.ValidationException;
 import Validator.Validator;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
 
 /**
  * Unit test for simple App.
@@ -24,23 +18,93 @@ public class AppTest {
     /**
      * Rigorous Test :-)
      */
+    private Validator<Student> studentValidator = new StudentValidator();
+
+    private StudentRepo rep = new StudentRepo(studentValidator, "studenti.xml");
+    private ServiceStudent srv = new ServiceStudent(rep);
+    private Student sampleStudent = new Student("1", "John Doe", 932, "john@doe.com", "Johnatan");
+
     @Test
     public void addStudent() {
-        Validator<Student> studentValidator = new StudentValidator();
 
-        StudentRepo rep = new StudentRepo(studentValidator, "studenti.xml");
-        ServiceStudent srv = new ServiceStudent(rep);
         assertEquals(new Student("123", "asda", 933, "asda@asda.casd", "asda"),
                 srv.add(new Student("123", "asda", 933, "asda@asda.casd", "asda")));
     }
 
     @Test
     public void addStudentInvalid() {
-        Validator<Student> studentValidator = new StudentValidator();
-
-        StudentRepo rep = new StudentRepo(studentValidator, "studenti.xml");
-        ServiceStudent srv = new ServiceStudent(rep);
         assertNotSame(new Student("123", "asda", 933, "asda@asda.casd", "asda"),
                 srv.add(new Student("124", "asda", 933, "asda@asda.casd", "asda")));
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addStudentNullId() {
+        Student student = sampleStudent;
+        student.setID(null);
+        srv.add(student);
+    }
+
+    //Solves for id == "" and length of id < 1
+    @Test(expected = ValidationException.class)
+    public void addStudentEmptyId() {
+        Student student = sampleStudent;
+        student.setID("");
+        srv.add(student);
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addStudentIdWithNotDigit() {
+        Student student = sampleStudent;
+        student.setID("91a");
+        srv.add(student);
+    }
+
+
+    @Test(expected = ValidationException.class)
+    public void addStudentWithWrongGroup1() {
+        Student student = new Student("1", "John Doe", 110, "john@doe.com", "Johnatan");
+        assertEquals(srv.add(student), student);
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addStudentWithWrongGroup2() {
+        Student student = new Student("1", "John Doe", 101, "john@doe.com", "Johnatan");
+        assertEquals(srv.add(student), student);
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addStudentWithWrongGroup3() {
+        Student student = new Student("1", "John Doe", 141, "john@doe.com", "Johnatan");
+        assertEquals(srv.add(student), student);
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addStudentWithWrongGroup4() {
+        Student student = new Student("1", "John Doe", 938, "john@doe.com", "Johnatan");
+        assertEquals(srv.add(student), student);
+    }
+
+    @Test
+    public void addStudentWithOkGroup1() {
+        Student student = new Student("1", "John Doe", 112, "john@doe.com", "Johnatan");
+        assertEquals(srv.add(student), student);
+    }
+
+    @Test
+    public void addStudentWithOkGroup2() {
+        Student student = new Student("1", "John Doe", 936, "john@doe.com", "Johnatan");
+        assertEquals(srv.add(student), student);
+    }
+
+    @Test
+    public void addStudentWithOkGroup3() {
+        Student student = new Student("1", "John Doe", 111, "john@doe.com", "Johnatan");
+        assertEquals(srv.add(student), student);
+    }
+
+    @Test
+    public void addStudentWithOkGroup4() {
+        Student student = new Student("1", "John Doe", 937, "john@doe.com", "Johnatan");
+        assertEquals(srv.add(student), student);
     }
 }
